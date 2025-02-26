cdd7274b18a74d361fd5f73b5a771cca5504ccf3
EDUARDO FEITOSA BATISTA <25132675@CFP127ALUNO.LOCAL>
2025-02-26 16:04:33 -0300

    Métodos de acesso set do peso e altura implementados

--- src/br/senai/sp/jandira/clinica/Clinica.java
+++ src/br/senai/sp/jandira/clinica/Clinica.java
@@ -19,8 +19,8 @@ public class Clinica {
 		Paciente p1 = new Paciente();
 		p1.nome = "Ana Maria";
 		p1.dataNascimento = LocalDate.of(2003, 7, 15);
-		p1.peso = 68.200;
-		p1.altura = 1.80;
+		p1.setPeso(68.200);
+		p1.setAltura(1.65);
 		p1.telefone = "(11)96666-6666";
 		p1.estaInternado = true;
 		
@@ -28,8 +28,8 @@ public class Clinica {
 		Paciente p2 = new Paciente();
 		p2.nome = "Mc Lovin";
 		p2.dataNascimento = LocalDate.of(1989, 6, 20);
-		p2.peso = 68.0389;
-		p2.altura = 1.77;
+		p2.setPeso(68);
+		p2.setAltura(1.77);
 		p2.telefone = "(11)98888-8888";
 		p2.estaInternado = false;
 		
